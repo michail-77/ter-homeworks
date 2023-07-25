1890e60e165fd272ddd8924b445f413e5cba7a03
Mikhail <michail.77@mail.ru>
2023-07-25 15:32:49 +0300

    11

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,7 +1,4 @@
 output "external_ip_vmweb"{
 value = yandex_compute_instance.vm_web.network_interface.0.nat_ip_address
-}
-
-output "external_ip_vmdb"{
 value = yandex_compute_instance.vm_db.network_interface.0.nat_ip_address
 }
